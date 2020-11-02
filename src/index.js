bb98d929ec76cefc9233da3d00627f50a1dabcde
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-02 11:25:34 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -24,8 +24,9 @@ import { render } from "react-dom";
 function App(props) {
   return (
     <div className="container">
+      {/* <h1>Welcome to React, Web {props.cohort}</h1> */}
       <h1>Welcome to React, Web {props.cohort}</h1>
-      <h1>Welcome to React, Web {props.cohort} I am {props.instructor}</h1>
+  <div>{instructor}</div>
     </div>
   );
 }
