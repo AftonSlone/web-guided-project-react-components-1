96e64e88e6c0b4471768b2e7fa00e850f34671d5
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-02 11:59:54 -0600

    wip

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -8,3 +8,4 @@
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
   - We use the dedicated "state updater" to schedule a state change: `setHealthPoints(healthPoints + 1)`
 */
+
